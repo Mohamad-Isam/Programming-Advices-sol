d6d5f33658aa59ee1fcb811a8a16d185e623d9b7
Mohamed Isam <116897631+Mohamed-Elagib@users.noreply.github.com>
2023-02-05 21:21:21 +0200

    Create 3.cpp

---
+++ 4.Algorithms & problem Solving level 1/3.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+enum enNumberType (Odd = 1, Even = 2);
+
+int readNumber()
+{
+  int x;
+  cout << "enter a number :\n"
+  cin >> x;
+  return x;
+}
+enNumberType checkNumberType(int num)
+{
+  int result = num % 2;
+  if(result == 0)
+    return enNumberType::Even;
+  else
+    return enNumberType::Odd;
+}
+
+void printNumberType(enNumberType numberType)
+{
+ if(numberType == enNumberType::Even)
+   cout << "even number" << endl;
+ else
+   cout << "odd number" << endl;  
+}
+
+int main()
+{
+  printNumberType(chackeNumberType(readNumber()));  
+  
+  return 0;
+}

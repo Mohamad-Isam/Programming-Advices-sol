8180f550643ecce3f211f363da7edc0a80aa7e18
Mohamed Isam <116897631+Mohamed-Elagib@users.noreply.github.com>
2023-02-05 21:09:17 +0200

    Create 1.cpp

---
+++ 4.Algorithms & problem Solving level 1/1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+void printName(string name)
+{
+  cout << "Your name is :\n" << name << endl;
+}
+
+int main()
+{
+  printName("Mohamed Isam");
+  return 0;
+}

1fb290b907ddfb83240c4f606b6c6e87e043769f
Mohamed Isam <116897631+Mohamed-Elagib@users.noreply.github.com>
2023-02-07 21:47:58 +0200

    Create 10.cpp

---
+++ 4.Algorithms & problem Solving level 1/10.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+struct stNumbers
+{
+    int num1;
+    int num2;
+    int num3;
+};
+
+stNumbers readNumbers()
+{
+    stNumbers numbers;
+    cout << "Enter first number : " << endl;
+    cin >> numbers.num1;
+    cout << "Enter second number : " << endl;
+    cin >> numbers.num2;
+    cout << "Enter third number : " << endl;
+    cin >> numbers.num3;
+
+    return numbers;
+}
+
+int sumNumbers(stNumbers numbers)
+{
+    return numbers.num1 + numbers.num2 + numbers.num3;
+}
+
+float avrNumbers(stNumbers numbers)
+{
+    return float(sumNumbers(numbers)) / 3;
+}
+
+void printResult(stNumbers numbers)
+{
+    cout << numbers.num1 << " + " << numbers.num2 << " + " << numbers.num3 << " = " << sumNumbers(numbers) << endl;
+    cout << "The average of the 3 numbers = " << avrNumbers(numbers) << endl;
+}
+
+int main()
+{
+    printResult(readNumbers());
+    return 0;
+}

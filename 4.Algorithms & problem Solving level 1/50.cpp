782908121a11dd6cff6ff196fbf6f183171d743f
Mohamed Isam <116897631+Mohamed-Elagib@users.noreply.github.com>
2023-03-27 22:31:42 +0200

    Create 50.cpp

---
+++ 4.Algorithms & problem Solving level 1/50.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int readPIN()
+{
+    int PIN;
+
+    cout << "Enter the PIN code : \n";
+    cin >> PIN;
+
+    return PIN;
+}
+
+bool Login()
+{
+    int PINcode, counter = 1;
+    do
+    {
+        PINcode = readPIN();
+        if(PINcode == 1234)
+            return true;
+        else
+        {
+            cout << "\nWrong PIN!!!\n";
+            system("color 4F");
+        }
+        counter++;
+    }while(PINcode != 1234 && counter <= 3);
+
+    return false;
+}
+
+int main()
+{
+    if(Login())
+    {
+        system("color 2F"); //turn screen to green.
+        cout << "\nYour total amount balance is " << 7500 << '\n';
+    }
+    else
+    {
+        cout << "account SUSPENDED , 3 times failed attempts\n";
+    }
+
+    return 0;
+}

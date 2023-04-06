9f31c40f2db3a6badb913b2fcef6db2ec078a21b
Mohamed Isam <116897631+Mohamad-Isam@users.noreply.github.com>
2023-04-06 17:38:25 +0200

    Create 39.cpp

---
+++ 5.Algorithms & problem Solving level 2/39.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int randomNumber(int From, int To)
+{
+    return rand() % (To - From + 1) + From;
+}
+
+vector<int> fillVector()
+{
+    vector<int> v;
+    for(int i = 0; i < 100; i++)
+        v.push_back(randomNumber(1, 100));
+    return v;
+}
+
+bool isPrime(int number)
+{
+    if(number == 0 || number == 1)
+        return false;
+    for(int i = 2; i <= number / 2; i++)
+    {
+        if(number % i == 0)
+            return false;
+    }
+    return true;
+}
+vector<int> addElements(vector<int> v1)
+{
+    vector<int> v2;
+
+    for(int num : v1)
+        if(isPrime(num))
+            v2.push_back(num);
+
+    return v2;
+}
+
+void printArray(vector<int > v)
+{
+    cout << "Array elements are : \n";
+    for(int x : v)
+        cout << x << ' ';
+    cout << '\n';
+}
+
+int main()
+{
+    srand((unsigned) time(NULL));
+    vector<int> v1, v2;
+    v1 = fillVector();
+    printArray(v1);
+    v2 = addElements(v1);
+    printArray(v2);
+
+
+    return 0;
+}
